4ae29b10ff7836260ac6e082c84af1b5a2e9ca60
DennySim <38099544+DennySim@users.noreply.github.com>
2022-03-22 09:31:34 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,19 @@
 # Provider
 terraform {
     required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-      version = "0.71.0"
-    }
-    hashicorp = {
-      source = "hashicorp/local"
-      version = "2.1.0"
-    }
-    hashicorp2 = {
-      source = "hashicorp/null"
-      version = "3.1.0"
-    } 
+        yandex = {
+          source = "yandex-cloud/yandex"
+          version = "0.71.0"
+        }
+        hashicorp = {
+          source = "hashicorp/local"
+          version = "2.1.0"
+        }
+        hashicorp2 = {
+          source = "hashicorp/null"
+          version = "3.1.0"
+        }
+    }    
 }
 
 provider "yandex" {
